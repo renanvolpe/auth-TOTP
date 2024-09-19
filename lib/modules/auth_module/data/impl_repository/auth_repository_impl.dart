e11b5ec41d9fa2c718107c63fb328d1ab5f45203
Renan Pereira Volpe <renanvolpe067@gmail.com>
2024-09-19 16:34:17 -0300

    feat: add data layer to get two endpoints login and secret key

---
+++ lib/modules/auth_module/data/impl_repository/auth_repository_impl.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:dartz/dartz.dart';
+import 'package:flutter_dev_test/modules/auth_module/data/abstract_repository/abstract_auth_repository.dart';
+import 'package:flutter_dev_test/modules/core/utils/endpoint.dart';
+import 'package:flutter_dev_test/modules/core/utils/failure.dart';
+
+import '../../../shared/dio/dio_service.dart';
+import '../param/login_param.dart';
+import '../param/recovery_secret_param.dart';
+
+class AuthRepositoryImpl implements IAuthRepository {
+  DioService dio;
+  AuthRepositoryImpl(this.dio);
+
+  @override
+  login(LoginParam loginParam) async{
+    try {
+      var response = await dio.dioPost(endpoint: Endpoint.login, body: loginParam.toMap());
+      return response.fold((failure) => failure, (success) {
+        String totpSecret = success["message"];
+        return Right(totpSecret);
+      });
+    } catch (e) {
+      return const Left(UnexpectedFailure("unexpected error in login, contact adm"));
+    }
+  }
+
+  @override
+  recoverySecret(RecoverySecretParam recoverySecretParam) async {
+    try {
+      var response = await dio.dioPost(endpoint: Endpoint.recoverySecret, body: recoverySecretParam.toMap());
+      return response.fold((failure) => failure, (success) {
+        String totpSecret = success["totp_secret"];
+        return Right(totpSecret);
+      });
+    } catch (e) {
+      return const Left(UnexpectedFailure("unexpected error in get recovery secret, contact adm"));
+    }
+  }
+}
